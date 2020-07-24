874e524c03d32325bd309bffad1c75401dd83ab5
Sandeep Sekhon <sandysss79@gmail.com>
2020-07-24 02:11:40 -0700

    completed section 9

---
+++ shared.js
@@ -0,0 +1,43 @@
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+let selectPlanButtons = document.querySelectorAll('.plan button');
+let modalNoButton = document.querySelector('.modal__action--negative');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
+
+for (let i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', function() {
+    // modal.style.display = 'block';
+    // backdrop.style.display = 'block';
+    // modal.className = 'open' //This will overwrite the complete class link
+    modal.classList.add('open');
+    backdrop.classList.add('open');
+  });
+}
+
+backdrop.addEventListener('click', function() {
+  // mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
+
+
+function closeModal() {
+  // modal.style.display = 'none';
+  // backdrop.style.display = 'none';
+  if (modal) {
+    modal.classList.remove('open');
+  }
+  backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function() {
+  // mobileNav.style.display = 'block';
+  // backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+})
